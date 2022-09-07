2897e07090bc12bd052580339eaf0059c12c85ec
Madalena Machado Rocha <rochamada1997m@gmail.com>
2022-09-07 16:37:48 -0300

    update image-gallery code

--- image-gallery/scripts.js
+++ image-gallery/scripts.js
@@ -1,16 +1,19 @@
 document.addEventListener('DOMContentLoaded', function() {
-  let stream = document.querySelector('.gallery__stream');
-  let items = document.querySelectorAll('.gallery__item');
+  let stream = document.querySelector('.gallery__stream')
+  let items = document.querySelectorAll('.gallery__item')
   
-  let prev = document.querySelector('.gallery__prev');
+  let prev = document.querySelector('.gallery__prev')
+  let next = document.querySelector('.gallery__next')
+
   prev.addEventListener('click', function() {
-    stream.insertBefore(items[items.length - 1], items[0]);
-    items = document.querySelectorAll('.gallery__item');
-  });
+    stream.insertBefore(items[items.length - 1], items[0])
+    // insere o items[9] antes do items[0] no stream, ou seja, pega a última imagem da galeria e insere no início da lista de imagens
+    items = document.querySelectorAll('.gallery__item')
+  })
   
-  let next = document.querySelector('.gallery__next');
   next.addEventListener('click', function() {
-    stream.appendChild(items[0]);
-    items = document.querySelectorAll('.gallery__item');
-  });
-});
+    stream.appendChild(items[0])
+    // adiciona o items[0] ao final do stream, ou seja, pega a primeira imagem da galeria e manda para o final da lista de imagens
+    items = document.querySelectorAll('.gallery__item')
+  })
+})
